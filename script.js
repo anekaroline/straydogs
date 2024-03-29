f51b4e522b7baec5e349c286c3655ac3f356e4ad
Ane Karoline da Silva Pereira <anekarolinespereira@gmail.com>
2024-03-29 16:36:08 -0300

    Criando carrossel em aumigos

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('showMenu');
 });
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("card-aumigos");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
